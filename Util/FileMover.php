fb81ed60656e33249706d377928b42165b989e9c
Andreas Tielitz <tielitz@thr3.de>
2016-03-13 20:02:52 +0100

    fixed mistakes and added license

--- Util/FileMover.php
+++ Util/FileMover.php
@@ -3,7 +3,9 @@
 namespace Gwinn\TinymceFastloadBundle\Util;
 
 use Symfony\Component\HttpFoundation\File\File;
+
 use Gwinn\TinymceFastloadBundle\FilenameGenerator\FilenameGeneratorInterface;
+use Gwinn\TinymceFastloadBundle\Exception\InvalidMappingException;
 
 class FileMover
 {
@@ -12,15 +14,24 @@ class FileMover
      */
     private $filenameGenerator;
 
-
+    /**
+     * Sets the use fileGenerator
+     *
+     * @param  string                  $filenameGenerator Classpath for the filenameGenerator
+     * @throws InvalidMappingException
+     */
     public function setFilenameGenerator($filenameGenerator)
     {
         if (class_exists($filenameGenerator)) {
             $refl = new \ReflectionClass($filenameGenerator);
             if ($refl->implementsInterface('Gwinn\TinymceFastloadBundle\FilenameGenerator\FilenameGeneratorInterface')) {
                 $this->filenameGenerator = $filenameGenerator;
+                return;
             }
         }
+
+        $msg = 'The FilenameGenerator for FileMover must be an existing class implementing FileGeneratorInterface.';
+        throw new InvalidMappingException($msg);
     }
 
     /**
@@ -32,16 +43,17 @@ class FileMover
      */
     public function move(File $file, $targetPath)
     {
-        $filenameWithoutExt = substr($file->getExtension(), 0, strrpos($file->getExtension(), '.'));
+        $filenameWithoutExt = substr($file->getFilename(), 0, strrpos($file->getFilename(), '.'));
         $fileExtension      = $file->getExtension();
 
-        $filename = $this->filenameGenerator->generate($filenameWithoutExt, $fileExtension, $file);
+        $generator = $this->filenameGenerator;
+        $filename  = $generator::generate($filenameWithoutExt, $fileExtension, $file);
 
         if (!file_exists($targetPath) && !is_dir($targetPath)) {
             $logger->info('TinyMCE creating directory '.$targetPath);
             mkdir($targetPath);
         }
 
-        return $fileOrig->move($targetPath, $filename);
+        return $file->move($targetPath, $filename);
     }
 }
