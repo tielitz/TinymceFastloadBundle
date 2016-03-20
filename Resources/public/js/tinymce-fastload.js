3fe932c3eff4a9149808f2f073016b316f728667
Johann Arendt <arendt@in.tum.de>
2016-03-20 13:04:57 +0100

    Adjustments for Notifications

--- Resources/public/js/tinymce-fastload.js
+++ Resources/public/js/tinymce-fastload.js
@@ -1,6 +1,3 @@
-var globalEd = undefined;
-
 function tinymce_button_image_uploader(ed) {
-    globalEd = ed;
     $('#tinymce_file_uploader input[type=file]').click();
 }
--- Resources/public/js/tinymce-image-upload.js
+++ Resources/public/js/tinymce-image-upload.js
@@ -1,6 +1,15 @@
 $('#tiny_inner_image').bind('change', function (event) {
+    // event.stopPropagation();
+    // event.preventDefault();
+
     formElement = document.getElementById("tinymce_file_uploader");
     data = new FormData(formElement);
+    ed = tinymce.get('news_type_text');
+
+    var tmpMsg = flashes.info('', 'Bild wird hochgeladen');
+    ed.getBody().setAttribute('contenteditable', 'false');
+    var tinyContainer = $('.mce-tinymce.mce-container.mce-panel');
+    tinyContainer.css({opacity: 0.5});
 
     $.ajax({
         url: $('#tinymce_file_uploader').attr('action'),
@@ -10,14 +19,21 @@ $('#tiny_inner_image').bind('change', function (event) {
         processData: false,
         success: function (response) {
             if (typeof(tinymce) != 'undefined') {
-                globalEd.selection.setContent(response);
+                ed.selection.setContent(response);
 
                 // Remove uploaded image, so it is possibly to reupload the same
                 $('#tiny_inner_image').closest('form').get(0).reset();
+                flashes.success('', 'Bild wurde hochgeladen');
             }
         },
         error: function () {
-            alert('Bild konnte nicht erfolgreich hochgeladen werden.');
+            // alert('Bild konnte nicht erfolgreich hochgeladen werden.');
+            flashes.danger('', 'Bild konnte nicht hochgeladen werden');
+        },
+        complete: function () {
+            ed.getBody().setAttribute('contenteditable', 'true');
+            tmpMsg.alert('close');
+            tinyContainer.css({opacity: 1});
         }
     });
 });
